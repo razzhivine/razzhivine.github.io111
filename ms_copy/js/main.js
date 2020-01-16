fea43fd936b47174704020dc2ea7bc268f9eccaa
Eugene <razzhivine@gmail.com>
2020-01-16 17:04:23 +0400

    Add files via upload

---
+++ ms_copy/js/main.js
@@ -0,0 +1,49 @@
+$('.single-slick').slick({
+  // slidesToShow: 1,
+  infinite: true,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  dots: true,
+  // asNavFor: '.slider-nav'
+});
+$('.btnPause').on('click', function () {
+  $('.single-slick').slickPause();
+});
+$('.btnStart').on('click', function () {
+  $('.single-slick').slickPlay();
+});
+var res = $(".drop_block");
+
+$(".drop").on("click", func);
+function func(){
+  if(res.css ("display") == "none"){
+    res.fadeIn(100);
+    res.css({'display':'flex'});
+  }
+  else{
+      res.fadeOut(100);
+  }
+}
+
+$(document).ready(function () {
+  $('.drop_list > li').click(function (event) {
+    $(this).children("ul").slideToggle();
+    event.stopPropagation();
+  });
+});
+
+  $(".action").click(function(){
+    $(".wrap, .input").toggleClass("active");
+    $("input[type='text']").focus();
+    $(".nav_item").hide();
+    $(".mob .fa-search").hide();
+  });
+  $(".close").click(function(){
+    $(".nav_item").show();
+    $(".mob .fa-search").show();
+    $(".wrap, .input").toggleClass("active");
+
+  });
